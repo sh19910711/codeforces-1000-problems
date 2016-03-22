604f9b8eb4e327f64e2b6204da6e88c24a2ed0b1
Hiroyuki Sano <sh19910711@gmail.com>
2016-03-22 00:59:02 +0000

    template: add main.cpp

---
+++ template/main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, n) for (int i = 0; i < n; ++i)
+
+#ifndef ONLINE_JUDGE
+#define debug_int(s) printf("%s = %d\n", #s, s)
+#else
+#define debug_int(s)
+#endif
+
+typedef long long int_t;
+
+bool input() {
+  return false;
+}
+
+void solve() {
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  while (input()) {
+    solve();
+  }
+  return 0;
+}
