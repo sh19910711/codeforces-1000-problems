7cf5ae49ba27cba9017194d59b2e6ebe97bc2434
Hiroyuki Sano <sh19910711@gmail.com>
2016-03-23 00:04:30 +0000

    template/main: rename debug_int() => print_int()

--- template/main.cpp
+++ template/main.cpp
@@ -4,9 +4,9 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < n; ++i)
 
 #ifndef ONLINE_JUDGE
-#define debug_int(s) printf("%s = %d\n", #s, s)
+#define print_int(s) printf("%s = %d\n", #s, s)
 #else
-#define debug_int(s)
+#define print_int(s)
 #endif
 
 typedef long long int_t;
